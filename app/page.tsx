378f838e64514a3beb81c8be2486050aea23c5a7
sukserhiy <sukserhiy@gmail.com>
2025-08-06 12:38:23 +0300

    add tailwind, shanui, loading, not-found

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Link from 'next/link'
- 
-export default function Page() {
+import Link from 'next/link';
+
+export default async function Page() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Link href="/about">About</Link>
     </div>
-  )
+  );
 }
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,8 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Muse';
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'A modern deezer-based project for music searching';
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
 export const DEEZER_API_HOST =
   process.env.DEEZER_API_HOST || 'https://api.deezer.com';
