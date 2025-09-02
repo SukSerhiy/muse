714e249a450892b03132a5c2b700a1b45ae5d4e8
sukserhiy <sukserhiy@gmail.com>
2025-09-02 14:26:07 +0300

    Configured eslint & prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,41 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import nextPlugin from '@next/eslint-plugin-next';
+import perfectionist from 'eslint-plugin-perfectionist';
+// eslint.config.js
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import tseslint from 'typescript-eslint';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: ['node_modules', '.next', 'dist'], // какие папки игнорить
+  },
+
+  // next + typescript
+  ...tseslint.configs.recommended,
+  // Use compatibility layer for Next shareable config (eslintrc-style)
+  ...compat.extends('next/core-web-vitals'),
+
+  // твои правила
+  {
+    plugins: {
+      '@next/next': nextPlugin,
+      perfectionist,
+    },
+    rules: {
+      semi: 'error',
+      quotes: ['error', 'single'],
+      'prefer-arrow-callback': ['error'],
+      'prefer-template': ['error'],
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      'no-console': ['warn'],
+      'perfectionist/sort-imports': ['error', { tsconfigRootDir: '.' }],
+      'perfectionist/sort-named-imports': ['error'],
+    },
+  },
 ];
 
 export default eslintConfig;
--- src/app/[locale]/(root)/page.tsx
+++ src/app/[locale]/(root)/page.tsx
@@ -1,9 +1,11 @@
-import { getTranslations } from "next-intl/server";
+import { getTranslations } from 'next-intl/server';
+
+import TrackQueue from '@/components/shared/TrackQueue';
+import { db } from '@/lib/db';
 import { getCharts } from '@/lib/external/services';
-import TrackQueue from "@/components/shared/TrackQueue";
-import { db } from "@/lib/db";
 import { Like } from '@/lib/types/track';
-import Albums from "./components/Albums";
+
+import Albums from './components/Albums';
 
 export default async function Page() {
   const chartsData = await getCharts();
@@ -22,14 +24,14 @@ export default async function Page() {
     return {
       ...item,
       like: currentLike,
-    }
+    };
   });
 
   const t = await getTranslations();
 
   return (
     <div>
-      <h1 className="text-3xl font-bold">{t("ChartsPage.title")}</h1>
+      <h1 className="text-3xl font-bold">{t('ChartsPage.title')}</h1>
       <div className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-4">
         <Albums albums={albums} />
       </div>
