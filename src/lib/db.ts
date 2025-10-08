a266c67f96a9a212fda8ad1d4d0b8577b957c7d7
sukserhiy <sukserhiy@gmail.com>
2025-10-08 14:53:42 +0300

    fix(build): fix for build

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,13 @@
 import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-const prisma = globalForPrisma.prisma || new PrismaClient();
+export const db =
+  global.prisma ??
+  new PrismaClient({
+    log: ['info', 'warn', 'error'],
+  });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
-
-export const db: PrismaClient = prisma;
+if (process.env.NODE_ENV !== 'production') global.prisma = db;
