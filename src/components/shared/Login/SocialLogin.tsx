c1790f7422bee95162f893cbd904d96883be0d54
sukserhiy <sukserhiy@gmail.com>
2025-10-14 12:49:34 +0300

    refactor(feature): refactor social login

--- src/components/shared/Login/SocialLogin.tsx
+++ src/components/shared/Login/SocialLogin.tsx
@@ -1,24 +1,10 @@
-'use client';
-import { useTheme } from 'next-themes';
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
 
 import { doSocialLogin } from '@/app/actions/auth.actions';
 const GoogleIcon = '/icons/google.svg';
 const GithubIcon = '/icons/github.svg';
 
 const SocialLogins = () => {
-  const { theme: defaultTheme } = useTheme();
-  const [theme, setTheme] = useState(defaultTheme);
-
-  const isDark = theme === 'dark';
-
-  useEffect(() => {
-    if (defaultTheme && theme !== defaultTheme) {
-      setTheme(defaultTheme);
-    }
-  }, [defaultTheme, theme]);
-
   return (
     <form action={doSocialLogin}>
       <button
@@ -27,17 +13,13 @@ const SocialLogins = () => {
         name="action"
         value="google"
       >
-        {defaultTheme != null ? (
-          <Image
-            src={GoogleIcon}
-            width={40}
-            height={40}
-            alt="google"
-            className={`h-full filter-[invert(${isDark ? 1 : 0})]`}
-          />
-        ) : (
-          <div className="min-h-[40px]" />
-        )}
+        <Image
+          src={GoogleIcon}
+          width={40}
+          height={40}
+          alt="google"
+          className="h-full invert-0 filter dark:invert"
+        />
       </button>
       <button
         className="hover:bg-accent m-1 ml-5 rounded-md p-1 transition-all duration-200"
@@ -45,17 +27,13 @@ const SocialLogins = () => {
         name="action"
         value="github"
       >
-        {defaultTheme != null ? (
-          <Image
-            src={GithubIcon}
-            width={40}
-            height={40}
-            alt="github"
-            className={`h-full filter-[invert(${isDark ? 1 : 0})]`}
-          />
-        ) : (
-          <div className="min-h-[40px]" />
-        )}
+        <Image
+          src={GithubIcon}
+          width={40}
+          height={40}
+          alt="github"
+          className="h-full invert-0 filter dark:invert"
+        />
       </button>
     </form>
   );
